ab6ccca920387bca1012856d85ef3640016bb396
Tim Cameron Ryan <id@timryan.org>
2013-02-20 01:50:19 -0500

    Diff

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function connect (pid, next) {
   console.error("Requesting auth tokens for " + pid + " from Lifegraph Connect...");
 
   rem.json('http://connect.lifegraphlabs.com/api/tokens/', pid).get({
-    namespace: 'entrance-tutorial',
-    key: "481848201872129",
-    secret: "f2696ba2416ae6a4cc9cbde1dddd6a5b"
+    namespace: _namespace,
+    key: _key,
+    secret: _secret
   }, function (err, json, res) {
     console.log("Response received from Lifegraph Connect.");
     console.log("Status Code: " + res.statusCode);

7ab2d67dd5b245ea30cdcf90f89d7ab98b1bd1d3
johnnyman727 <jonathan.mckay@students.olin.edu>
2013-02-24 12:51:26 -0500

    Added error if lifegraph isn't configured properly

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ function configure (_namespace, _key, _secret) {
 function connect (pid, next) {
   console.error("Requesting auth tokens for " + pid + " from Lifegraph Connect...");
 
+  if (!namespace || !key || !secret) {
+    console.log("Attempting to connect to lifegraph without properly configuring namespace, key, or secret.");
+
+    next(new Error("Need to configure lifegraph before connecting."), null);
+  }
+
   rem.json('http://connect.lifegraphlabs.com/api/tokens/', pid).get({
     namespace: _namespace,
     key: _key,
